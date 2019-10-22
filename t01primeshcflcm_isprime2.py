d195aa3605f237ff0c11a44d8ace23668b1e223b
dhslauren <38091569+dhslauren@users.noreply.github.com>
2019-10-22 14:09:11 +0800

    Update t01primeshcflcm_isprime2.py

--- t01primeshcflcm_isprime2.py
+++ t01primeshcflcm_isprime2.py
@@ -1,4 +1,23 @@
-# Is num a prime number? 
+num = 407
+= int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+# take input from the user
+# num # Is num a prime number? 
 # A prime number is an integer with exactly 2 different factors, 1 and itself.
 # Can you rectify the following program error?
 
